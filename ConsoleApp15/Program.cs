3fdfb14d61acc9692356035bab4fe52a3a0b0f6a
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-15 13:39:32 +0300

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var currentYear = DateTime.Now.Year;
+        var birthYear = 1993;
+        int age = currentYear - birthYear;
+
+        Console.WriteLine("Ai implinit " + age + " de ani!");
+
+        if (age == 30)
+        {
+            Console.WriteLine("La multi ani!");
+        }
+        else if (age < 30)
+        {
+            int yearsLeft = 30 - age;
+            Console.WriteLine("Mai ai de asteptat " + yearsLeft + " ani!");
+        }
+        else
+        {
+            Console.WriteLine("Ai trecut de 30 de ani!");
+        }
+    }
+}
